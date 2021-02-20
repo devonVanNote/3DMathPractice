beec224a26338aee7a9c8a315631f499d345173c
Devon VanNote <dvon5150@gmail.com>
2021-02-20 17:06:09 -0600

    Bitwise operations WIP

---
+++ Assets/Scripts/AttributeManager.cs
@@ -0,0 +1,56 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AttributeManager : MonoBehaviour
+{
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
+
+    public Text attributeDisplay;
+    int attributes = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case Tag.Magic:
+                attributes |= MAGIC;
+                break;
+            case Tag.Intelligence:
+                attributes |= INTELLIGENCE;
+                break;
+            case Tag.Charisma:
+                attributes |= CHARISMA;
+                break;
+           case Tag.Fly:
+                attributes |= FLY;
+                break;
+            case Tag.Invisible:
+                attributes |= INVISIBLE;
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+        attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
+    }
+       
+}
---
+++ Assets/Scripts/Enums/Tags.cs
@@ -0,0 +1,8 @@
+public class Tag 
+{   
+    public const string Magic = "MAGIC";
+    public const string Intelligence = "INTELLIGENCE";
+    public const string Charisma = "CHARISMA";
+    public const string Fly = "FLY";
+    public const string Invisible = "INVISIBLE";
+}
