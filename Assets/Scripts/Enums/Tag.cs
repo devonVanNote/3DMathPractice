f2e4b1e9c0103ed4b4084b2cdbc504fe78aa7ff4
Devon VanNote <dvon5150@gmail.com>
2021-03-07 19:51:14 -0600

    Added tags back

---
+++ Assets/Scripts/Enums/Tag.cs
@@ -0,0 +1,25 @@
+public class Tag
+{
+    public const string Magic = "MAGIC";
+    public const string Intelligence = "INTELLIGENCE";
+    public const string Charisma = "CHARISMA";
+    public const string Fly = "FLY";
+    public const string Invisible = "INVISIBLE";
+    public const string Add = "ADD";
+    public const string Remove = "REMOVE";
+    public const string Reset = "RESET";
+    public const string MagicDoor = "MAGIC_DOOR";
+    public const string InvisibleDoor = "INVISIBLE_DOOR";
+    public const string CharismaDoor = "CHARISMA_DOOR";
+    public const string GoldenKey = "GOLDEN_KEY";
+    public const string MagicKey = "MAGIC_KEY";
+    public const string InvisibleKey = "INVISIBLE_KEY";
+    public const string CharismaKey = "CHARISMA_KEY";
+    public const string Dirt = "Dirt";
+    public const string Desert = "Desert";
+    public const string Grain = "Grain";
+    public const string Pasture = "Pasture";
+    public const string Rock = "Rock";
+    public const string Water = "Water";
+    public const string Woods = "Woods";
+}
